e5050d088e75e9498c5d2ba46c37fcfae685592a
Grzesiek <grzegorzociepka09@gmail.com>
2019-10-12 15:54:55 +0200

    11/0 fix

--- 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie00.js
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie00.js
@@ -1,27 +1,26 @@
 //0a
-function show(e){
-    e.target.querySelector('.children').style.display="block";
+function show(e) {
+    e.target.querySelector('.children').style.display = "block";
 }
-function hide(e){
-    e.target.querySelector('.children').style.display="none";
+function hide(e) {
+    e.target.querySelector('.children').style.display = "none";
 }
 
 const parents = document.querySelectorAll('.parent');
 
-for(let item of parents){
-    item.addEventListener('mouseenter',show,false);
+for (let item of parents) {
+    item.addEventListener('mouseenter', show, false);
 }
 
 //0b
 
-for(let parent of parents){
-    parent.addEventListener("mouseleave", function() {
-        
-        function hideChildrenElement(el){
+for (let parent of parents) {
+    parent.addEventListener("mouseleave", function () {
 
-            el.querySelector('.children').style.display='none';
+        function hideChildrenElement() {
+            this.querySelector('.children').style.display = "none";
         }
-        hideChildrenElement(this);
+        hideChildrenElement.call(this);
     })
 }
 
