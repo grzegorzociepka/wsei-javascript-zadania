392188ce61e70e527a820e5a40a1f5e4c47dab93
Grzesiek <grzegorzociepka09@gmail.com>
2019-10-12 14:56:56 +0200

    additional/tooltips added

--- 1_Zadania/dodatkowe/5_Tooltips/js/app.js
+++ 1_Zadania/dodatkowe/5_Tooltips/js/app.js
@@ -1,3 +1,16 @@
-/*
- <span class="tooltipText">Text tooltipa</span>
- */
+document.addEventListener('DOMContentLoaded', () => {
+    const newElement = document.createElement('span');
+    newElement.classList.add('tooltipText');
+    newElement.innerText = 'Text tooltipa';
+    const tooltips = document.querySelectorAll('.tooltip');
+    tooltips.forEach(tooltip => {
+        tooltip.addEventListener('mouseover', (e) => {
+            console.log(e.currentTarget.getAttribute('data-text'));
+            e.currentTarget.appendChild(newElement);
+        });
+        tooltip.addEventListener('mouseout', (e) => {
+            console.log(e.currentTarget.getAttribute('data-text'));
+            e.currentTarget.removeChild(newElement);
+        });
+    })
+});
